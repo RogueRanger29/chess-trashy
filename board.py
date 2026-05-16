be88165b305b899cd14a3688c610d8a8c79f596e
ARNAVster <jainkarnav29@gmail.com>
2026-05-16 22:16:28 +0800

    Draw basic board

--- board.py
+++ board.py
@@ -1,5 +1,32 @@
 import pygame
-from settings import WIDTH, HEIGHT
+from helper import WIDTH, HEIGHT, LIGHT, DARK, DEFAULT_BOARD
 
-def draw_board(screen: pygame.surface.Surface):
-    pass
+board = DEFAULT_BOARD.copy()
+def draw_board(screen: pygame.Surface):    
+    square_width = WIDTH/8
+    square_height = HEIGHT/8
+    for row in range(8):
+        for col in range(8):
+            square = pygame.Rect(col*square_width, row*square_height, square_width, square_height)
+            if (row+col)%2:
+                color = DARK
+            else:
+                color = LIGHT
+            
+            pygame.draw.rect(screen, color, square)
+            
+def draw_pieces(screen: pygame.Surface):
+    piece_width = WIDTH/8
+    piece_height = HEIGHT/8
+    piece_size = (piece_width, piece_height)
+    for row, rank in enumerate(board):
+        for col, piece in enumerate(rank):
+            if piece != ' ':
+                if piece.isupper():
+                    piece_image = pygame.image.load(f'whitepiece/{piece}.png').convert_alpha()
+                    piece_image = pygame.transform.scale(piece_image, piece_size)
+                    screen.blit(piece_image, (col*piece_width, row*piece_height))
+                else:
+                    piece_image = pygame.image.load(f'blackpiece/{piece}.png').convert_alpha()
+                    piece_image = pygame.transform.scale(piece_image, piece_size)
+                    screen.blit(piece_image, (col*piece_width, row*piece_height))
--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import pygame
 import sys
-from settings import SIZE, FPS
-
+from helper import SIZE, FPS
+from board import draw_board, draw_pieces
 pygame.init()
 
 screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
 
 run = True
-print(type(screen))
+    
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             
-    
+    draw_board(screen)
+    draw_pieces(screen)
+    pygame.display.flip()
     clock.tick(FPS)
 pygame.quit()
 sys.exit()
