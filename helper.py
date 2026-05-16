d24a3d0572cd5b70901b13e1a58e8d3869b9a399
ARNAVster <jainkarnav29@gmail.com>
2026-05-16 22:19:57 +0800

    Woops! forgot helper

--- helper.py
+++ helper.py
@@ -2,3 +2,24 @@ WIDTH = 800
 HEIGHT = 800
 FPS = 24
 SIZE = (WIDTH, HEIGHT)
+
+#colors
+LIGHT = (238, 238, 210)
+DARK = (118, 150, 86)
+
+FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'
+
+def fen_to_board(fen: str):
+    res = []
+    segments = fen.split('/')
+    for segment in segments:
+        rank = ''
+        for c in segment:
+            if c.isdigit():
+                rank += int(c)*' '
+            else:
+                rank+=c
+        res += [list(rank)]
+    return res
+
+DEFAULT_BOARD = fen_to_board(FEN)
