32bda1dd8127b458b0891ad1246bd53d4d89593b
ARNAVster <jainkarnav29@gmail.com>
2026-05-19 23:12:45 +0800

    added movement mostly correct movements for pawn, knight, king and rook

--- board.py
+++ board.py
@@ -10,6 +10,7 @@ selected_col = -1
 selected_piece = ""
 
 turn = "w"
+turns = 0
 
 def set_click_state(click: bool):
     global clicking
@@ -42,11 +43,132 @@ def draw_pieces(screen: pygame.Surface):
                     screen.blit(piece_image, (col*piece_width, row*piece_height))
                     
                     
-def colour(piece):
+def colour(piece: str):
+    if piece == " ":
+        return ""
     return "w" if piece.isupper() else "b"
+
+def check_legal_moves(r: int, c: int):
+    res = []
+    p = board[r][c]
+    #PAWN - TODO: En Passant
+    if p.lower() == "p": #checks if it is A pawn
+        if colour(p) == "w":  #checks for white pawn
+            if r-1 >= 0: #checks that there is a spot above
+                #move conditons
+                if board[r-1][c] == " ": #checks that the spot is empty
+                    res.append((r-1, c)) #add the spot above
+                    
+                    if r == 6 and board[r-2][c] == " ": #if unmoved, allow for 2 move
+                       res.append((r-2, c))
+                
+                #take conditions
+                if c+1 <= 7 and colour(board[r-1][c+1]) == "b":
+                    res.append((r-1, c+1))
+                if c-1 >=0 and colour(board[r-1][c-1]) == "b":
+                    res.append((r-1, c-1))
+        if colour(p) == "b":  #checks for white pawn
+            if r+1 <= 7: #checks that there is a spot above
+                #move conditons
+                if board[r+1][c] == " ": #checks that the spot is empty
+                    res.append((r+1, c)) #add the spot above
+                    
+                    if r == 1 and board[r+2][c] == " ": #if unmoved, allow for 2 move
+                       res.append((r+2, c))
+                
+                #take conditions
+                if c+1 <= 7 and colour(board[r+1][c+1]) == "w":
+                    res.append((r+1, c+1))
+                if c-1 >=0 and colour(board[r+1][c-1]) == "w":
+                    res.append((r+1, c-1))
+                    
+    #knight
+    if p.lower() == "n":
+        temp = []
+        temp.append((r+1, c+2))
+        temp.append((r+1, c-2))
+        temp.append((r-1, c+2))
+        temp.append((r-1, c-2))
+        temp.append((r+2, c+1))
+        temp.append((r+2, c-1))
+        temp.append((r-2, c+1))
+        temp.append((r-2, c-1))
+        for move in temp:
+            if move[0] < 0 or move[0] > 7  or move[1] < 0 or move[1] > 7:
+                continue
+            if colour(board[move[0]][move[1]]) == colour(p): #ur own pieces
+                continue
+            res.append(move)
+    
+    #king
+    if p.lower() == "k":
+        temp = []
+        temp.append((r-1, c-1))
+        temp.append((r-1, c))
+        temp.append((r-1, c+1))
+        temp.append((r, c-1))
+        temp.append((r, c+1))
+        temp.append((r+1, c-1))
+        temp.append((r+1, c))
+        temp.append((r+1, c+1))
+        
+        for move in temp:
+            if move[0] < 0 or move[0] > 7  or move[1] < 0 or move[1] > 7:
+                continue
+            if colour(board[move[0]][move[1]]) == colour(p): #ur own pieces
+                continue
+            res.append(move)
+    
+    #rook
+    if p.lower() == "r":
+        #check right
+        for i in range(1,8):
+            if c+i > 7:
+                break
+            if colour(board[r][c+i]) == colour(p):
+                break
+            if (colour(board[r][c+i]) == "w" and colour(p) == "b") or (colour(board[r][c+i]) == "b" and colour(p) == "w"):
+                res.append((r, c+i))
+                break
+            res.append((r, c+i))
+        
+        #check left
+        for i in range(1,8):
+            if c-i < 0:
+                break
+            if colour(board[r][c-i]) == colour(p):
+                break
+            if (colour(board[r][c-i]) == "w" and colour(p) == "b") or (colour(board[r][c-i]) == "b" and colour(p) == "w"):
+                res.append((r, c-i))
+                break
+            res.append((r, c-i))
+            
+        #check down
+        for i in range(1,8):
+            if r+i > 7:
+                break
+            if colour(board[r+i][c]) == colour(p):
+                break
+            if (colour(board[r+i][c]) == "w" and colour(p) == "b") or (colour(board[r+i][c]) == "b" and colour(p) == "w"):
+                res.append((r+i, c))
+                break
+            res.append((r+i, c))
+            
+        #check up
+        for i in range(1,8):
+            if r-i < 0:
+                break
+            if colour(board[r-i][c]) == colour(p):
+                break
+            if (colour(board[r-i][c]) == "w" and colour(p) == "b") or (colour(board[r-i][c]) == "b" and colour(p) == "w"):
+                res.append((r-i, c))
+                break
+            res.append((r-i, c))
+    return res
+
                     
 def move():
-    global selected_row, selected_col, selected_piece, turn
+    global selected_row, selected_col, selected_piece, turn, turns
     if clicking:
         clicked_row = int(pygame.mouse.get_pos()[1]//SQUARE_HEIGHT)
         clicked_col = int(pygame.mouse.get_pos()[0]//SQUARE_WIDTH)
@@ -59,21 +181,22 @@ def move():
                 selected_piece=clicked_piece
                 selected_piece = clicked_piece
                 overlayboard[clicked_row][clicked_col] = 1
-        else:
+                print(check_legal_moves(selected_row, selected_col))
+        else: #move/change selection
             if (clicked_row, clicked_col) == (selected_row, selected_col): #deselect piece
                 overlayboard[selected_row][selected_col] = 0
                 selected_row = -1
                 selected_col = -1
                 selected_piece = ""
             
-            elif colour(clicked_piece) == turn and clicked_piece != " ":
+            elif colour(clicked_piece) == turn and clicked_piece != " ": #change selection
                 overlayboard[selected_row][selected_col] = 0
                 selected_row = clicked_row
                 selected_col = clicked_col
                 selected_piece = clicked_piece
                 overlayboard[clicked_row][clicked_col] = 1
             
-            elif clicked_piece == " " or colour(clicked_piece) !=  colour(board[selected_row][selected_col]): #check whether its empty space there or opponent colour
+            elif (clicked_row, clicked_col) in check_legal_moves(selected_row, selected_col): #check whether its empty space there or opponent colour
                 board[clicked_row][clicked_col] = selected_piece
                 board[selected_row][selected_col] = " "
                 overlayboard[selected_row][selected_col] = 0
@@ -81,6 +204,7 @@ def move():
                 selected_col = -1
                 selected_piece = ""
                 turn = "b" if turn=="w" else "w"
+                turns += 1
         
 def draw_overlay(screen: pygame.Surface):
     for r, rank in enumerate(overlayboard):
