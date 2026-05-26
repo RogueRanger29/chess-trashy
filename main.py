3aff4110a78214119708932abef41dc12d2fce41
noyn noyn <81067285+NoynYT@users.noreply.github.com>
2026-05-26 18:18:27 +0800

    other

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ window_handlery = pygame.display.get_wm_info()["window"]
 try:
     u32.ShowWindow(window_handlery, 9)
     u32.SetForegroundWindow(window_handlery)
-    u32.SetWindowPos(window_handlery, -1, 0,0,0,0, 0x0002|0x001|0x0040)
+    u32.SetWindowPos(window_handlery, -1, 0,0,0,0, 0x0002|0x0001)
 except:
     print("no console window handle found.")
 
